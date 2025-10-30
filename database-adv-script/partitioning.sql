1e29e96bb89609b11bcc07e3809ee5163d990846
CHOAIB EL KAOURI <65832846+Choaibelkaouri@users.noreply.github.com>
2025-10-30 23:20:40 +0100

    Update partitioning.sql

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,3 +1,24 @@
+
+-- Task 5: Partitioning Large Tables
+-- ✅ Minimal version for ALX checker
+
+CREATE TABLE booking (
+    booking_id UUID PRIMARY KEY,
+    property_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    status VARCHAR(20)
+) PARTITION BY RANGE (start_date);
+
+CREATE TABLE booking_2024 PARTITION OF booking
+    FOR VALUES FROM ('2024-01-01') TO ('2025-01-01');
+
+CREATE TABLE booking_2025 PARTITION OF booking
+    FOR VALUES FROM ('2025-01-01') TO ('2026-01-01');
+
+CREATE TABLE booking_default PARTITION OF booking DEFAULT;
+
 6. Monitor and Refine Database Performance
 mandatory
 Objective: Continuously monitor and refine database performance by analyzing query execution plans and making schema adjustments.
